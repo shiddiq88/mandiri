994bd0d6d60de76b0b5d4527140309a047ac25ff
shiddiq88 <rz.shiddiq@gmail.com>
2019-08-07 19:07:59 +0700

    add curency and cart

--- js/init.js
+++ js/init.js
@@ -1,3 +1,4 @@
+// firebase init
 const firebaseConfig = {
   apiKey: "AIzaSyDIbhtaPL4HMKD5IdSMo2jVQRWKeXvqljg",
   authDomain: "bengkel-mandiri-dep.firebaseapp.com",
@@ -8,29 +9,58 @@ const firebaseConfig = {
   appId: "1:931944146437:web:bb2ce70e03f3bca7"
 };
  firebase.initializeApp(firebaseConfig);
+var db = firebase.database();
 
-
-var reff = firebase.database().ref("/part");
-reff.once("value")
-.then(function(snapshot){
-	var data = snapshot.val();
-	var key = Object.keys(data);
-	key.forEach(function(item,index){
-		let parts = data[item];
-		let partsKeys = Object.keys(parts);
-		let selector = '#'+item
-		partsKeys.forEach(function(item2,index2){
-		$(selector).append(
+// funtion currency to IDR
+var IDR = value => currency(value, { symbol: 'Rp ', decimal: ',', separator: '.' });
+// daftar spare part
+let daftarHarga={};
+var partReff = db.ref("/part");
+partReff.on('value', function(snapshot){
+	let part = snapshot.val()
+	Object.keys(part).forEach(function(item,index){
+		let spart = part[item]
+		Object.keys(spart).forEach(function(item2,index2){
+		let harga = IDR(spart[item2]).format(true);
+		$('#'+item).append(
 			` <tr>
-                  <td>`+ item2 +`</td>
-                  <td> RP. `+ parts[item2] +`</td>
+                  <td id="`+item2+`">`+ item2 +`</td>
+                  <td>`+ harga +`</td>
                   <td><a href=""></a><i class="material-icons red-text">add_circle</i></td>
                </tr>
 			`)
+		daftarHarga[item2]=spart[item2];
 		})
-	});
+	}) 
+console.log(daftarHarga)
+// ui keranjang
+let cartReff = db.ref("/cart")
+cartReff.on('value', function(snapCart){
+	let cart = (snapCart.exists()) ? snapCart.val() : []
+	let jmlCart = item.length == 0 ? '' : Object.keys(item).length
+	$('#count-'+item).text(jmlCart)
+		Object.keys(item).forEach(function(item2){
+			$('#list-keranjang').append(
+			` <tr>
+                  <td>`+ item2 +`</td>
+                  <td>`+ 2 +`</td>
+                  <td><a href=""></a><i class="material-icons red-text">add_circle</i></td>
+               </tr>
+			`)
+// console.log(cart[item])
+// console.log(item)
+// console.log(item2)
+
+		})	
+	})
 })
 
-$(document).ready(function(){
-    $('.collapsible').collapsible();
-  });
+
+
+// aktivasi collapsible materializ css
+M.AutoInit();
+// $(document).ready(function(){
+//   $('.collapsible').collapsible();
+//   $('.tabs').tabs();
+//   });
+
