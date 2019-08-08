17b3f22b51c2a49924bc1f1f61e90c56deaf6eda
shiddiq88 <rz.shiddiq@gmail.com>
2019-08-08 07:35:55 +0700

    mandiri-app v0.2

--- js/init.js
+++ js/init.js
@@ -13,48 +13,32 @@ var db = firebase.database();
 
 // funtion currency to IDR
 var IDR = value => currency(value, { symbol: 'Rp ', decimal: ',', separator: '.' });
-// daftar spare part
+
 let daftarHarga={};
 var partReff = db.ref("/part");
 partReff.on('value', function(snapshot){
 	let part = snapshot.val()
+	let tempObj = {}
+	
+	let arrayCart = Object.keys(tempObj)
+	console.log(arrayCart)
 	Object.keys(part).forEach(function(item,index){
 		let spart = part[item]
 		Object.keys(spart).forEach(function(item2,index2){
 		let harga = IDR(spart[item2]).format(true);
 		$('#'+item).append(
 			` <tr>
-                  <td id="`+item2+`" class="desc">`+ item2 +`</td>
+                  <td id="`+item2.split(' ').join('-')+`" class="desc">`+ item2 +`</td>
                   <td>`+ harga +`</td>
-                  <td><a href=""></a><i class="material-icons red-text partList">add_circle</i></td>
+                  <td class="btnList"><a href=""></a><i class="material-icons orange-text partList">add_circle</i></td>
                </tr>
 			`)
 		daftarHarga[item2]=spart[item2];
 		})
-	}) 
-})
-// tambah cart
-$(document).on('click','.partList', function(){
-	let namaPart =$(this).parent().siblings(".desc").text() 
-	let jml = prompt("masukan jumlah barang!")
-	db.ref('/cart/'+namaPart).set(parseInt(jml))
-})
-
-// hapus cart
-$(document).on('click','.hapus', function(){
-	let namaPart =$(this).parent().siblings(".desc").text() 
-	db.ref('/cart/'+namaPart).set(null)
-})
-
-// ubah Qty
-$(document).on('click','.qty', function(){
-	let namaPart =$(this).siblings(".desc").text() 
-	let jml = prompt("rubah Qty "+namaPart)
-	db.ref('/cart/'+namaPart).set(parseInt(jml))
+	})
 })
 
 
-console.log(daftarHarga)
 // ui keranjang
 let cartReff = db.ref("/cart")
 cartReff.on('value', function(snapCart){
@@ -70,13 +54,37 @@ cartReff.on('value', function(snapCart){
                   <td class='qty'>`+ cart[item] +`</td>
                   <td>`+ IDR(daftarHarga[item]).format(true)+`</td>
                   <td> `+ IDR(cart[item]*daftarHarga[item]).format(true) +`</td>
-                  <td><a href=""></a><i class="material-icons yellow-text hapus" >cancel</i></td>
+                  <td><a href=""></a><i class="material-icons red-text hapus" >cancel</i></td>
                </tr>
 			`)
 	total += cart[item]*daftarHarga[item]
+	$('#'+item.split(' ').join('-')).siblings('.btnList').html('<a href=""></a><i class="material-icons green-text" >check_circle</i>')
+	console.log($('#'+item.split(' ').join('-')).siblings('.btnList').html())
 	})	
-	console.log(total)
 	$("#total-belanja").text( IDR(total).format(true) )
+})
+
+// tambah cart
+$(document).on('click','.partList', function(){
+	let namaPart =$(this).parent().siblings(".desc").text() 
+	let jml = prompt("masukan jumlah barang!")
+	db.ref('/cart/'+namaPart).set(parseInt(jml))
+})
+
+// hapus cart
+$(document).on('click','.hapus', function(){
+	let namaPart =$(this).parent().siblings(".desc").text() 
+	db.ref('/cart/'+namaPart).set(null)
+	let selectorDecs = $(this).parents('td').siblings('.desc').text()
+	$('#'+selectorDecs.split(' ').join('-')).siblings('.btnList').html('<a href=""></a><i class="material-icons orange-text partList">add_circle</i>')
+
+})
+
+// ubah Qty
+$(document).on('click','.qty', function(){
+	let namaPart =$(this).siblings(".desc").text() 
+	let jml = prompt("rubah Qty "+namaPart)
+	db.ref('/cart/'+namaPart).set(parseInt(jml))
 })
 
 // aktivasi materializ css
