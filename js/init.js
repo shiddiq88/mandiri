d240f71577b5ea004ca4bcb9ab7cdfc31b459f16
shiddiq88 <rz.shiddiq@gmail.com>
2019-08-09 13:47:28 +0700

    include sweetalert2

--- js/init.js
+++ js/init.js
@@ -38,7 +38,6 @@ partReff.on('value', function(snapshot){
 	})
 })
 
-
 // ui keranjang
 let cartReff = db.ref("/cart")
 cartReff.on('value', function(snapCart){
@@ -59,7 +58,6 @@ cartReff.on('value', function(snapCart){
 		`)
 	total += cart[item]*daftarHarga[item]
 	$('#'+item.split(' ').join('-')).siblings('.btnList').html('<a href=""></a><i class="material-icons green-text" >check_circle</i>')
-	// console.log($('#'+item.split(' ').join('-')).siblings('.btnList').html())
 	})	
 	$("#total-belanja").text( IDR(total).format(true) )
 })
@@ -67,52 +65,126 @@ cartReff.on('value', function(snapCart){
 // event tombol tambah cart
 $(document).on('click','.partList', function(){
 	let namaPart =$(this).parent().siblings(".desc").text() 
-	let jml = prompt("masukan jumlah barang!")
-	db.ref('/cart/'+namaPart).set(parseInt(jml))
+	async function start() {
+		const {value: number} = await Swal.fire({
+		  title: 'Jumlah '+namaPart+' :',
+		  input: 'text',
+		  inputPlaceholder: 'masukan jumlah yang diinginkan!',
+	 	  showCancelButton: true ,
+  		  inputValidator : value => {
+    		if (!value) {
+		  	return 'jumlah tidak boleh kosong'
+    	  	} else if (isNaN(value)) {
+		  	return 'input mengandung karakter terlarang!'
+		  	} 
+  		}
+	})
+
+	if (number) {
+		Swal.fire('berhasil menambah ' + namaPart+' sebanyak '+ number)
+		db.ref('/cart/'+namaPart).set(parseInt(number))
+	}}
+	start()
 })
 
 // event tombol hapus cart
 $(document).on('click','.hapus', function(){
 	let namaPart =$(this).parent().siblings(".desc").text() 
-	db.ref('/cart/'+namaPart).set(null)
 	let selectorDecs = $(this).parents('td').siblings('.desc').text()
-	$('#'+selectorDecs.split(' ').join('-')).siblings('.btnList').html('<a href=""></a><i class="material-icons orange-text partList">add_circle</i>')
-
+	Swal.fire({
+	  title: 'Anda yakin?',
+	  text: "apakah anda yakin menghapu "+namaPart+' dari cart',
+	  type: 'warning',
+	  showCancelButton: true,
+	  confirmButtonColor: '#3085d6',
+	  cancelButtonColor: '#d33',
+	  confirmButtonText: 'ya, saya yakin!'
+		}).then((result) => {
+		  if (result.value) {
+		    Swal.fire({
+		    	type: 'success',   
+				title: namaPart + 'telah dihapus dari cart',
+				showConfirmButton: false,
+				timer: 1500 })
+				db.ref('/cart/'+namaPart).set(null)
+				$('#'+selectorDecs.split(' ').join('-')).siblings('.btnList').html('<a href=""></a><i class="material-icons orange-text partList">add_circle</i>')
+		  }
+	})
 })
 
 // event tombol ubah Qty
 $(document).on('click','.qty', function(){
 	let namaPart =$(this).siblings(".desc").text() 
-	let jml = prompt("rubah Qty "+namaPart)
-	if (isNaN(jml)) {
-		alert('jml mengandung karakter terlarang')
-	} else {
-	db.ref('/cart/'+namaPart).set(parseInt(jml))	
+	let qty =$(this).text() 
+	
+	async function start() {
+		const {value: number} = await Swal.fire({
+		  title: 'Jumlah '+namaPart+' :',
+		  input: 'text',
+		  // inputPlaceholder: qty,
+		  inputValue: qty,
+	 	  showCancelButton: true ,
+  		  inputValidator: (value) => {
+    		if (!value) {
+		  	return 'jumlah tidak boleh kosong'
+    	  	} else if (isNaN(value)) {
+		  	return 'input mengandung karakter terlarang!'
+		  	} 
+  		  }
+		})
+
+		if (number == 0) {
+			db.ref('/cart/'+namaPart).set(null)
+			Swal.fire({
+			    	type: 'success',   
+					title: 'berhasil menghapus ' + namaPart,
+					showConfirmButton: false,
+					timer: 1500 })
+
+		} else if (number > 0){
+			db.ref('/cart/'+namaPart).set(parseInt(number))
+			Swal.fire({
+			    	type: 'success',   
+					title: 'berhasil merubah qty ' + namaPart+' menjadi '+ number,
+					showConfirmButton: false,
+					timer: 1500 })
+		}
 	}
+	start()
 })
 
 // event tombol tambah data 
 $(document).on('click','#btn-tambah-data',function(){
-	// document.location.href = '#!'
 	let desc = $('#desc-tambah-data').val()
 	let kategori = $('#kategori-tambah-data').val()
 	let price = $('#price-tambah-data').val()
-//	$('#modal1').close()
     if (desc == '' || price == '' ){
-    	alert('nama part atau harga tidak boleh kosong')
+    	Swal.fire('Kesalahan input !','nama part atau harga tidak boleh kosong','error')
     } else if ( !desc.match(/^[A-Za-z]+$/) || isNaN(price) ) {
-    	alert('nama part atau harga mengaandung karakter terlarang')
+    	Swal.fire('Kesalahan input !','nama part atau harga mengandung karakter terlarang','error')
     } else {
-	    let conf = confirm('nama : '+desc+'\n kategori : '+kategori+'\n harga : '+price+'\n apakah anda yakin?')
-	    if (conf) {
+	    Swal.fire({
+		  title: 'Anda yakin?',
+		  text: "apakah anda yakin menambah "+desc+' dengan kategori : '+kategori+' dan harga : '+IDR(price).format(true),
+		  type: 'warning',
+		  showCancelButton: true,
+		  confirmButtonColor: '#3085d6',
+		  cancelButtonColor: '#d33',
+		  confirmButtonText: 'ya, saya yakin!'
+			}).then((result) => {
+			  if (result.value) {
+			    Swal.fire({
+			    	type: 'success',   
+					title: desc+' dengan kategori : '+kategori+' dan harga : '+IDR(price).format(true)+' telah ditambahkan',
+					showConfirmButton: false,
+					timer: 1500 })
+			  }
 			db.ref('/part/'+kategori+'/'+desc).set(parseInt(price))
 			window.location.reload()
-	    } 	
+		})
     }
 
 })
-
-
 
 // aktivasi materializ css
 M.AutoInit();
